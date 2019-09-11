7ec7dbe57fcf2ee11a948f62b961b730eeb424f0
박효진 <hgenie14@gmail.com>
2019-09-11 18:15:49 +0900

    [#5] feat: 사용자가 보낸 메시지를 받는 기능 구현

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 app.use(express.json()); // 이걸 안해주니까 요청에서 body를 못찾아
 
 /** routes **/
 const subscriptions = require('./routes/subscriptions');
+const message = require('./routes/message');
 
-app.set("port", (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use('/subscriptions', subscriptions);
+app.use('/message', message);
 
-app.get("/", function (req, res) {
-    res.send("<h1>MARTIN BOT</h1>");
+app.get('/', function (req, res) {
+    res.send('<h1>MARTIN BOT</h1>');
 });
 
-
-app.listen(app.get("port"), () =>
-    console.log("APP IS RUNNING ON [" + app.get("port") + "]")
+app.listen(app.get('port'), () =>
+    console.log('APP IS RUNNING ON [' + app.get('port') + ']')
 );
