ffdb6d634cea3a6036bdc8925908f55341169d46
박효진 <hgenie14@gmail.com>
2019-09-11 15:44:52 +0900

    feat: Event Subscriptions의 유효한 요청 Url 등록

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+app.use(express.json()); // 이걸 안해주니까 요청에서 body를 못찾아
 
 app.set("port", (process.env.PORT || 3000));
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
+});
+
+app.post('/action-endpoint', function (req, res) {
+    console.log(req.body);
+    res.status(200).json(req.body);
 });
 
 app.listen(app.get("port"), function () {
