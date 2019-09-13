ac16a6d879f3750c5070c7de0b65f2f80ef608fa
박효진 <hgenie14@gmail.com>
2019-09-13 17:31:45 +0900

    [#7] 메시지를 보낸 채널에 랜덤으로 메시지 응답하는 기능 구현

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -3,15 +3,50 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 
+const request = require('request');
+const token = process.env.TOKEN;
+
+const positiveMessages = [
+    '마자마자~', '그럴 수 있지', '그럼그럼', '그치그치',
+    '좋은데?', '괜찮은데?', '나쁘지 않은데?', '오키오키',
+    ':martin:', ':sunshine:'];
+
+let options = {
+    uri: 'https://slack.com/api/chat.postMessage',
+    qs: {
+        token: token
+    }
+};
+
 router.post('/action-endpoint', function (req, res) {
     const reqBody = req.body;
     console.log(reqBody);
 
     if (reqBody.event.type === 'message') {
-        const channel_id = reqBody.event.channel;
+        sendReaction(reqBody.event);
     }
 
     res.status(200).json(reqBody.body);
 });
+
+const sendReaction = function (event) {
+    const channel_id = event.channel;
+    const messageIndex = makeRandomNumberLessThan(positiveMessages.length);
+
+    options.qs.channel = channel_id;
+    options.qs.text = positiveMessages[messageIndex];
+
+    request.post(options, function (err, response, body) {
+        if (err) {
+            console.error("err >> " + err);
+            return;
+        }
+        console.log(body);
+    });
+}
+
+const makeRandomNumberLessThan = function (maxNumber) {
+    return Math.floor(Math.random() * maxNumber);
+}
 
 module.exports = router;
