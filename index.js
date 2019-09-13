4567185d009340f04da047749af5f2cc48746a8a
박효진 <hgenie14@gmail.com>
2019-09-13 17:26:14 +0900

    [#6] feat: 메시지를 보낸 채널 및 DM의 id 받기

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-app.use(express.json()); // 이걸 안해주니까 요청에서 body를 못찾아
+app.use(express.json());
 
-/** routes **/
 const subscriptions = require('./routes/subscriptions');
-const message = require('./routes/message');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/subscriptions', subscriptions);
-app.use('/message', message);
 
 app.get('/', function (req, res) {
     res.send('<h1>MARTIN BOT</h1>');
--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 
 router.post('/action-endpoint', function (req, res) {
-    console.log(req.body);
-    res.status(200).json(req.body);
+    const reqBody = req.body;
+    console.log(reqBody);
+
+    if (reqBody.event.type === 'message') {
+        const channel_id = reqBody.event.channel;
+    }
+
+    res.status(200).json(reqBody.body);
 });
 
 module.exports = router;
