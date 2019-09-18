59625cc7a25025aff066f620af0bb8f96bc03287
박효진 <hgenie14@gmail.com>
2019-09-19 02:16:21 +0900

    fix: 댓글이 달린 경우에도 응답하는 오류 수정

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,11 +1,18 @@
-require('dotenv').config();
+require('dotenv').config({
+    path: './config/.env'
+});
 
 const express = require('express');
 const router = express.Router();
 
 const request = require('request');
+const moment = require('moment');
+require('moment-timezone');
+
 const token = process.env.TOKEN;
 const bot_id = process.env.BOT_ID;
+
+moment.tz.setDefault("Asia/Seoul");
 
 const positiveMessages = [
     '마자마자~', '그럴 수 있지', '그럼그럼', '그치그치',
@@ -21,25 +28,31 @@ let options = {
 
 router.post('/action-endpoint', function (req, res) {
     const reqBody = req.body;
-
+    let result;
+    
     if (reqBody.event.type === 'message' && reqBody.event.bot_id !== bot_id) {
-        sendReaction(reqBody.event);
+        result = sendReaction(reqBody.event);
     }
+    
+    console.log("[" + moment().format('YYYY-MM-DD HH:mm:ss') + "] " 
+                + reqBody.event.channel_type + ": " + reqBody.event.user + " -> " + result);
 
-    res.status(200).json(reqBody.body);
+    res.status(200).json(result);
 });
 
 const sendReaction = function (event) {
+    if (event.thread_ts) {
+        return '댓글이란다~';
+    }
+    if (makeRandomNumberLessThan(10) < 8) {
+        return '아무 말도 하지 않겠다';
+    }
+    
     const channel_id = event.channel;
     const messageIndex = makeRandomNumberLessThan(positiveMessages.length);
 
     options.qs.channel = channel_id;
     options.qs.text = positiveMessages[messageIndex];
-
-    if (makeRandomNumberLessThan(10) < 8) {
-        console.log('아무 말도 하지 않겠다');
-        return;
-    }
 
     request.post(options, function (err, response, body) {
         if (err) {
@@ -48,6 +61,8 @@ const sendReaction = function (event) {
         }
         console.log(body);
     });
+    
+    return positiveMessages[messageIndex];
 }
 
 const makeRandomNumberLessThan = function (maxNumber) {
