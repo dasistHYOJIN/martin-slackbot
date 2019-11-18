e22373dbab29fbd0798cb37080be9e10bac87075
박효진 <hgenie14@gmail.com>
2019-11-18 23:19:58 +0900

    refactor: subscriptions.sendReaction() 메서드 분리

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -6,16 +6,8 @@ const express = require('express');
 const router = express.Router();
 
 const request = require('request');
-const {
-    createLogger,
-    format,
-    transports
-} = require('winston');
-const {
-    combine,
-    label,
-    printf
-} = format;
+const { createLogger, format, transports } = require('winston');
+const { combine, label, printf } = format;
 const moment = require('moment');
 require('moment-timezone');
 
@@ -24,10 +16,7 @@ const bot_id = process.env.BOT_ID;
 
 moment.tz.setDefault("Asia/Seoul");
 
-const logFormat = printf(({
-    level,
-    message
-}) => `[${timeStampFormat()}] ${level}: ${message}`);
+const logFormat = printf(({level, message}) => `[${timeStampFormat()}] ${level}: ${message}`);
 const logger = createLogger({
     format: logFormat,
     transports: [
@@ -42,7 +31,7 @@ const logger = createLogger({
     new transports.File({
             filename: 'logs/logs.log'
         }),
-      new transports.Console()
+    new transports.Console()
   ]
 });
 
@@ -51,19 +40,12 @@ const positiveMessages = [
     '좋은데?', '괜찮은데?', '나쁘지 않은데?', '오키오키',
     ':martin:', ':sunshine:'];
 
-let options = {
-    uri: 'https://slack.com/api/chat.postMessage',
-    qs: {
-        token: token
-    }
-};
-
 router.post('/action-endpoint', function (req, res) {
     const reqBody = req.body;
     let result;
 
     if (reqBody.event.type === 'message' && reqBody.event.bot_id !== bot_id) {
-        result = sendReaction(reqBody.event, makeRandomNumberLessThan(10));
+        result = processReaction(reqBody.event, makeRandomNumberLessThan(10));
     }
 
     const logMessage = "TYPE=" + reqBody.event.type + " SUBTYPE=" + reqBody.event.subtype + " USERNAME=" + reqBody.event.username + " CHANNEL_TYPE=" + reqBody.event.channel_type + " RESULT_TEXT=" + result;
@@ -72,7 +54,7 @@ router.post('/action-endpoint', function (req, res) {
     res.status(200).json(result);
 });
 
-const sendReaction = function (event, number) {
+const processReaction = function (event, number) {
     if (event.thread_ts) {
         return '댓글이란다~';
     }
@@ -80,22 +62,29 @@ const sendReaction = function (event, number) {
         return '아무 말도 하지 않겠다';
     }
 
-    const channel_id = event.channel;
     const messageIndex = makeRandomNumberLessThan(positiveMessages.length);
+    let options = {
+        uri: 'https://slack.com/api/chat.postMessage',
+        qs: {
+            token: token,
+            channel: event.channel,
+            text: positiveMessages[messageIndex]
+        }
+    };
+    sendReaction(options, messageIndex);
 
-    options.qs.channel = channel_id;
-    options.qs.text = positiveMessages[messageIndex];
+    return positiveMessages[messageIndex];
+}
 
+const sendReaction = function (options, messageIndex) {
     request.post(options, function (err, response, body) {
         if (err) {
             logger.error(err);
             return;
         }
-        
+
         logger.info("'" + positiveMessages[messageIndex] + "'라고 맞장구 완료");
     });
-
-    return positiveMessages[messageIndex];
 }
 
 const makeRandomNumberLessThan = function (maxNumber) {
